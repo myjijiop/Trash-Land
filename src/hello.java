6eee800a42bb3ccf5cedf13abcd51b24857f974e
Jerry Xue <guangjiexue1@gmail.com>
2019-08-29 10:10:53 -0700

    initial commit

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         System.out.println("Yo mama is kinda nice");
+        System.out.println("gina told me to do this");
     }
 }
